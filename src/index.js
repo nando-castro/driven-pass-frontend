98ba0743e7b77e6cc21768900cc37d8374debe2f
nando-castro <fernando.castro10.master@gmail.com>
2022-09-11 23:49:08 -0300

    chore: context auth

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { AuthProvider } from "./context/auth";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+);
